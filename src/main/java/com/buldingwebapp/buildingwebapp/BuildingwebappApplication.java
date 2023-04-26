672b190bf41738246b042b45c51e4c546d207b92
davidjesusruiz <davidjesus.ruiz@nucleoo.com>
2023-04-26 13:34:02 +0200

    deleteUser test and createTestUser added

--- src/main/java/com/buldingwebapp/buildingwebapp/BuildingwebappApplication.java
+++ src/main/java/com/buldingwebapp/buildingwebapp/BuildingwebappApplication.java
@@ -1,13 +1,8 @@
 package com.buldingwebapp.buildingwebapp;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.buldingwebapp.buildingwebapp")
--- src/test/java/com/buldingwebapp/buildingwebapp/BuildingwebappApplicationTests.java
+++ src/test/java/com/buldingwebapp/buildingwebapp/BuildingwebappApplicationTests.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.Optional;
 
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class BuildingwebappApplicationTests {
 
 	@Autowired
@@ -18,21 +16,37 @@ class BuildingwebappApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	@Test
-	void testUpdateUser(){
+
+	Long createTestUser(){
 		User user = new User("Test", "test@example.com");
 		userRepository.save(user);
-		Optional<User> optional_user = userRepository.findById(user.getId());
+		return user.getId();
+	}
+
+	@Test
+	void testUpdateUser(){
+		Long idTest = createTestUser();
+		Optional<User> optional_user = userRepository.findById(idTest);
 		if (optional_user.isPresent()){
 			User userToUpdate = optional_user.get();
 			userToUpdate.setEmail("userupdated@gmail.com");
 			userToUpdate.setName("Updated");
 			userRepository.save(userToUpdate);
-			optional_user = userRepository.findById(user.getId());
+			optional_user = userRepository.findById(idTest);
 			if (optional_user.isPresent()){
 				Assertions.assertEquals("userupdated@gmail.com",optional_user.get().getEmail());
 				Assertions.assertEquals("Updated",optional_user.get().getName());
 			}
+			userRepository.deleteById(idTest);
 		}
+	}
+
+	@Test
+	void testDeleteUser(){
+		Long idTest = createTestUser();
+		Optional<User> optional_user = userRepository.findById(idTest);
+		optional_user.ifPresent(user -> userRepository.deleteById(user.getId()));
+		optional_user = userRepository.findById(idTest);
+		Assertions.assertTrue(optional_user.isEmpty());
 	}
 }
